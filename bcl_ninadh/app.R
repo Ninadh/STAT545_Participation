91e01330cf82b08aac749f99e8b9ea664764545c
Ninadh D'Costa <ninadhmalrinadcosta@gmail.com>
2018-11-15 11:00:18 -0800

    cm108 end of class

--- bcl_ninadh/app.R
+++ bcl_ninadh/app.R
@@ -7,31 +7,38 @@ bcl <- read.csv("~/Desktop/STAT545/STAT545_Participation/bcl_ninadh/bcl-data.csv
 
 ui <- fluidPage(
   titlePanel("BC Liquor price app", 
-                           windowTitle = "BCL app"),
-                sidebarLayout(
-                  sidebarPanel(sliderInput("priceInput", "Select your desired price range.",
-                                           min = 0, max = 100, value = c(15, 30), pre="$"),
-                  radioButtons("typeInput", "Select your beverage price range", 
-                               choices = c("Beer", "Refreshment", "Spirit", "Wine"),
-                               selected = "Wine")
-                  ),
-                  mainPanel(
-                    plotOutput("price_hist"),
-                    tableOutput("BCL_data")
-                )
-                )
+             windowTitle = "BCL app"),
+  sidebarLayout(
+    sidebarPanel(sliderInput("priceInput", "Select your desired price range.",
+                             min = 0, max = 100, value = c(15, 30), pre="$"),
+                 radioButtons("typeInput", "Select your beverage price range", 
+                              choices = c("Beer", "Refreshment", "Spirit", "Wine"),
+                              selected = "Wine")
+    ),
+    mainPanel(
+      plotOutput("price_hist"),
+      tableOutput("BCL_data")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$price_hist <- renderPlot({
+  observe(print(input$priceInput))
+  bcl_filtered <-  reactive({
     bcl %>%
-      filter( Price < input$priceInput[2],
-             Price > input$priceInput[1]) %>%
-    ggplot(aes(Price)) +
+      filter(Price < input$priceInput[2],
+             Price > input$priceInput[1],
+             Type == input$typeInput)
+  })
+  output$price_hist <- renderPlot({
+    bcl_filtered() %>%
+      ggplot(aes(Price)) +
       geom_histogram ()
-    })
-  output$BCL_data <- renderTable(bcl)
+  })
+  output$price_hist <- renderTable({
+    bcl_filtered()
+  })
 }
 
 # Run the application 
